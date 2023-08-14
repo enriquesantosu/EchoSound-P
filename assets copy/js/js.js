82ea5cf4da3495bc2aaa286c8c377c401faf35cb
enriquesantosu <enriquesantosu@gmail.com>
2023-08-14 12:52:34 -0500

    first commit

---
+++ assets copy/js/js.js
@@ -0,0 +1,13 @@
+// const btn = document.querySelector("#buttonNav");
+// const icon = document.querySelector(".fa-solid");
+
+// console.log(btn, icon);
+
+// btn.addEventListener("click", () => {
+//     icon.classList.add('active');
+
+//     setTimeout(() => {
+//         icon.classList.remove('active');
+//     }, 50);
+
+// });
